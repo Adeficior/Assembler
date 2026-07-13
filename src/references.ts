16a88665e24f7ab7d33681fe62c892967167119f
Niklas Widmann <nik@somethingcatchy.net>
2026-07-13 19:29:01 +0200

    also clone neoforge data

--- src/references.ts
+++ src/references.ts
@@ -1,3 +1,4 @@
+import { parseSemVer } from "@adeficior/data-modifier";
 import { ensureDir, type Logger } from "@adeficior/pack-resolver";
 import { $ } from "bun";
 import { existsSync } from "node:fs";
@@ -5,30 +6,49 @@ import { join } from "node:path";
 import type { Options } from "./args";
 
 const referencesRepo = "https://github.com/misode/mcmeta.git";
+const neoforgeRepo = "https://github.com/neoforged/NeoForge.git";
 
 export default async function cloneReferences({ pack, dirs, logger }: Options) {
   await ensureDir(dirs.references);
 
   logger.info("cloning references...");
 
-  await Promise.all([
+  const promises = [
     cloneReference(
       dirs.references,
+      referencesRepo,
       "vanilla-assets",
       `${pack.versions.minecraft}-assets-json`,
       logger,
     ),
     cloneReference(
       dirs.references,
+      referencesRepo,
       "vanilla-data",
       `${pack.versions.minecraft}-data-json`,
       logger,
     ),
-  ]);
+  ];
+
+  if (pack.versions.neoforge) {
+    const { major, minor } = parseSemVer(pack.versions.neoforge);
+    promises.push(
+      cloneReference(
+        dirs.references,
+        neoforgeRepo,
+        "neoforge",
+        `${major}.${minor}`,
+        logger,
+      ),
+    );
+  }
+
+  await Promise.all(promises);
 }
 
 async function cloneReference(
   referencesDir: string,
+  repo: string,
   name: string,
   tag: string,
   logger: Logger,
@@ -38,7 +58,7 @@ async function cloneReference(
     return;
   }
 
-  await $`git clone --depth 1 --branch ${tag} ${referencesRepo} ${name}`
+  await $`git clone --depth 1 --branch ${tag} ${repo} ${name}`
     .quiet()
     .cwd(referencesDir);
 
